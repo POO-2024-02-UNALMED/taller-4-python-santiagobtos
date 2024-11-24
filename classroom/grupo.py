fd62f984b2801fde93daad1fd3027881e9af255a
santiagobtos <sbarrientosm@unal.edu.co>
2024-11-24 02:07:49 +0000

    as

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,23 +1,27 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "Grado 12"
+    grado = "grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
+    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None  ):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self.asignaturas = asignaturas if asignaturas is not None else []
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None: 
+            lista = []
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        for elemento in lista:
+            if elemento not in self.listadoAlumnos:
+                self.listadoAlumnos.append(elemento)
 
     def __str__(self):
-        if self._grupo== "grupo predeterminado":
+        if self._grupo== "grupo ordinado":
             cadena= "Grupo de estudiantes: grupo predeterminado"
             return cadena
         else :
